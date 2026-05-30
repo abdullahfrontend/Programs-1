f73f5cb084dc549e2d8ba2a78819c57632733fe6
Abdullah Abbasi <167675560+abdullahfrontend@users.noreply.github.com>
2026-05-30 19:15:17 +0500

    Update Program to print the right most digit of a number.py

--- Program to print the right most digit of a number.py
+++ Program to print the right most digit of a number.py
@@ -1,3 +1,7 @@
+# This program finds and displays the rightmost (last) digit of a given number. 
+# It uses the modulus (%) operator to extract the last digit.
+# Output:
+# The last digit of the entered number.
 # Language : Python
 
 num = int(input("Enter a Number"))
