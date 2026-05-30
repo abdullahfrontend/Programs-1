80eee116553bc026d617243ac9ab71e91a864797
Abdullah Abbasi <167675560+abdullahfrontend@users.noreply.github.com>
2026-05-30 19:13:40 +0500

    Update Chess Board and Rice Problem.js

--- Chess Board and Rice Problem.js
+++ Chess Board and Rice Problem.js
@@ -1,6 +1,7 @@
 // This program demonstrates exponential growth by placing rice grains on a chessboard. 
 // The first square contains 1 grain, and each subsequent square contains twice as many grains as the previous one. 
 // The program calculates the number of grains on each square and the total number of grains on the chessboard.
+// Language : JavaScript
 
 let rice = 2;
 
